a00dcd53b7bc18ad0716153ed216a0ddca422835
이준희 <junh0328@naver.com>
2020-11-18 15:19:17 +0900

    ColorConsumer 컴포넌트 추가를 통한 동적인 상태관리

--- src/components/SelectColors.js
+++ src/components/SelectColors.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ColorConsumer } from "../contexts/color";
 
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
@@ -6,20 +7,31 @@ const SelectColors = () => {
   return (
     <div>
       <h2>색상을 선택하세요.</h2>
-      <div style={{ display: "flex" }}>
-        {colors.map((color) => (
-          <div
-            key={color}
-            style={{
-              background: color,
-              width: "24px",
-              height: "24px",
-              cursor: "pointer",
-              marginRight: "5px",
-            }}
-          />
-        ))}
-      </div>
+      <ColorConsumer>
+        {({ actions }) => (
+          <div style={{ display: "flex" }}>
+            {colors.map((color) => (
+              <div
+                key={color}
+                style={{
+                  background: color,
+                  width: "24px",
+                  height: "24px",
+                  cursor: "pointer",
+                  marginRight: "5px",
+                }}
+                // 마우스 좌클릭
+                onClick={() => actions.setColor(color)}
+                // 마우스 우클릭 시 메뉴가 뜨는 것을 무시함, 우클릭으로 setSubcolor를 조정하기 위해서
+                onContextMenu={(e) => {
+                  e.preventDefault();
+                  actions.setSubcolor(color);
+                }}
+              />
+            ))}
+          </div>
+        )}
+      </ColorConsumer>
       <hr />
     </div>
   );
